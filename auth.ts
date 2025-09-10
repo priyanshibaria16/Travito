abed43dc6cd31c0fc98a6d12912557737d68bd09
priyanshibaria16 <priyanshibaria05@gmail.com>
2025-09-11 01:09:18 +0530

    components

--- auth.ts
+++ auth.ts
@@ -4,6 +4,12 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [GitHub],
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
   adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET, // always add this
 });
